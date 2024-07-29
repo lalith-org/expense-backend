b46984957673dc3c3e385358a736705a561a1493
sailalith <lalithvoop@hotmail.com>
2024-07-29 20:26:55 +0530

    pushing tag 1.0.1 in BE

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+
 // ROUTES FOR OUR API
 // =======================================================
 
